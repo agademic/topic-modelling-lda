0d4a5d78b770a5b27d03a693f1889053a6d4f2bb
agademic <arthur.gogohia@gmail.com>
2020-11-17 14:18:16 +0100

    initial commit

---
+++ model_training.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 12:11:24 2020
+
+@author: a.gogohia
+"""
+
+
+import json
+import gensim
+from pre_processing import (create_bow,
+                            create_tfidf)
+
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+##### CREATE TOPIC MODELS #####
+    
+def create_lda_model(corpus, id2word, num_topics=10, passes=10):
+    lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=num_topics, 
+                                           update_every=1,
+                                           chunksize=100,
+                                           passes=passes,
+                                           alpha='auto',
+                                           per_word_topics=True)
+    return lda_model
+    
+def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
+    """
+    Compute c_v coherence for various number of topics
+
+    Parameters:
+    ----------
+    dictionary : Gensim dictionary
+    corpus : Gensim corpus
+    texts : List of input texts
+    limit : Max num of topics
+
+    Returns:
+    -------
+    model_list : List of LDA topic models
+    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    coherence_values = []
+    model_list = []
+    for num_topics in range(start, limit, step):
+        model = gensim.models.ldamodel.LdaModel(corpus, num_topics=num_topics,
+                                       id2word=dictionary, passes=10)
+        model_list.append(model)
+        coherencemodel = gensim.models.CoherenceModel(model=model, texts=texts,
+                                                      dictionary=dictionary,
+                                                      coherence='c_v')
+        coherence_values.append(coherencemodel.get_coherence())
+
+    return model_list, coherence_values
+
+if __name__ == '__main__':
+    
+    # READ PRE-PROCESSED DATA
+    with open('processed_data.txt', 'r') as f:
+        processed_data = json.load(f)
+        
+    # CREATE DICTIONARY AND CORPUS
+    id2word, corpus = create_bow(processed_data) # bag of words
+    id2word, tfidf_corpus = create_tfidf(processed_data) # tf-idf
+    
+    # CHECK FREQUENT TOKENS
+    dfs_desc = sorted(id2word.dfs.items(), key=lambda t: t[1], reverse=True)
+    
+    print('### Most Frequent Tokens in', id2word.num_docs, 'Documents ###')
+    for (k,v) in dfs_desc[0:10]: print('{freq}: {token}'.format(token=id2word[k], freq=v))
+    
+    print('### Least Frequent Tokens in', id2word.num_docs, 'Documents ###')
+    for (k,v) in dfs_desc[-10:]: print('{freq}: {token}'.format(token=id2word[k], freq=v))
+
+    # TRAIN SINGLE MODEL
+    lda_model = create_lda_model(corpus=corpus, id2word=id2word, num_topics=10)
+    # OR 
+    # lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+    #                                        id2word=id2word,
+    #                                        num_topics=10, 
+    #                                        random_state=100,
+    #                                        update_every=1,
+    #                                        chunksize=100,
+    #                                        passes=10,
+    #                                        alpha='auto',
+    #                                        per_word_topics=True)
+
+    # PRINT TOPICS
+    lda_model.print_topics()
+
+    # COMPUTE COHERENCE SCORE
+    coherence_score = gensim.models.CoherenceModel(model=lda_model, texts=processed_data, dictionary=id2word, coherence='c_v').get_coherence()
+    print(f'### The coherence score for the single model is {coherence_score}. ###')
+
+    # ITERATE OVER POSSIBLE NUMBER OF TOPICS
+    # model_list, coherence_values = compute_coherence_values(dictionary=id2word, corpus=corpus, texts=processed_data, start=5, limit=50, step=5)
+
+
+    
+    
+    
+    
---
+++ pre_processing.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  4 11:18:01 2020
+
+@author: a.gogohia
+"""
+
+import json
+import pandas as pd
+import nltk
+import de_core_news_md # spacy german language model
+import re
+import gensim
+
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+##### REMOVE NOISE #####
+
+def remove_emails(sentences):
+    """Remove all emails from list/series of sentences"""
+    new_sentences = [re.sub('\S*@\S*\s?', '', sentence) for sentence in sentences]
+    return new_sentences
+
+def remove_new_line_chars(sentences):
+    """Remove all new line characters from list/series of sentences"""
+    new_sentences = [re.sub('\s+', '', sentence) for sentence in sentences]
+    return new_sentences
+
+def remove_single_quote_chars(sentences):
+    """Remove all single quote characters from list/series of sentences"""
+    new_sentences = [re.sub('\'', '', sentence) for sentence in sentences]
+    return new_sentences
+
+def remove_numbers(sentences):
+    """Remove all numbers from text"""
+    new_sentences = [re.sub(r'[0-9]+', '', sentence) for sentence in sentences]
+    return new_sentences
+
+def remove_punctuation(sentences):
+    """Remove punctuation from text"""
+    new_sentences = [re.sub(r'[^\w\s]+', '', sentence) for sentence in sentences]
+    return new_sentences
+
+def remove_noise(data):
+    data = remove_emails(data)
+    data = remove_single_quote_chars(data)
+    data = remove_numbers(data)
+    data = remove_punctuation(data)
+    return data
+
+##### NORMALIZE #####
+
+def to_lowercase(sentences):
+    """Convert all characters to lowercase from list/series of sentences"""
+    new_sentences = [sentence.lower() for sentence in sentences]
+    return new_sentences
+
+def remove_stopwords(sentences):
+    """Remove stopwords from list/series of sentences"""
+    new_sentences = [[word for word in doc.split() if word not in stopwords] for doc in sentences]
+    return new_sentences
+
+def lemmatize(sentences, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """Lemmatize all words i.e. gefunden -> finden"""
+    texts_out = []
+    nlp = de_core_news_md.load()
+    for sent in sentences:
+        doc = nlp(sent) 
+        texts_out.append(' '.join([token.lemma_ for token in doc if token.pos_ in allowed_postags]))
+    return texts_out
+
+def normalize(data):
+    data = lemmatize(data)
+    data = to_lowercase(data)
+    data = remove_stopwords(data)
+    return data
+
+##### TOKENIZE #####
+
+def tokenize(sentences):
+    new_sentences = [sentence.split() for sentence in sentences]
+    return new_sentences
+
+##### CREATE DICTIONARY AND CORPUS #####
+
+def create_bow(texts, verbose=True):
+    id2word = gensim.corpora.Dictionary(texts)
+    freq_before = len(id2word)
+    id2word.filter_extremes(no_below=1, no_above=0.9, keep_n=100000)
+    corpus = [id2word.doc2bow(text) for text in texts]
+    if verbose == True:
+        print('### Filtering least and most occuring tokens ###')
+        print(f'{freq_before} tokens before filtering -> {len(id2word)} after filtering')
+    return id2word, corpus
+
+def create_tfidf(texts):
+    id2word, corpus = create_bow(texts)
+    tfidf = gensim.models.TfidfModel(corpus)
+    tfidf_corpus = tfidf[corpus]
+    return id2word, tfidf_corpus
+
+
+if __name__ == '__main__':
+    
+    # READ DATA
+    data = pd.read_csv('data/articles.csv', sep='\n', header=None)
+    data = data[0]
+    
+    stopwords = nltk.corpus.stopwords.words('german')
+    
+    # PRE-PROCESS DOCUMENTS
+    processed_data = remove_noise(data)
+    processed_data = normalize(processed_data)
+    # processed_data = tokenize(processed_data) # not needed since normalize() function returns tokens
+
+    # SAVE FILE
+    with open('processed_data.txt', 'w') as f:
+        json.dump(new_data, f)
